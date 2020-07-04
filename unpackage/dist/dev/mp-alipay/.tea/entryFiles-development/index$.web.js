d36b6f04404c2c61c0436a59ee9c022346651cb6
lee <820952660@qq.com>
2020-07-05 00:01:35 +0800

    构建项目

---
+++ unpackage/dist/dev/mp-alipay/.tea/entryFiles-development/index$.web.js
@@ -0,0 +1,7 @@
+require('@alipay/appx-compiler/lib/sjsEnvInit');
+require('./config$');
+
+require('../../pages/tabBar/home/home?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
+require('../../pages/tabBar/category/category?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
+require('../../pages/tabBar/cart/cart?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
+require('../../pages/tabBar/user/user?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
